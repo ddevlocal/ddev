07e10df30b9b5efbbf7a4c398256b7e07dff1cc0
Randy Fay <randy@randyfay.com>
2018-11-28 14:02:55 -0700

    Experiment with not trying to mount the current directory, instead mount /Users/rfay in sanetestbot.sh

--- .buildkite/sanetestbot.sh
+++ .buildkite/sanetestbot.sh
@@ -10,7 +10,7 @@ set -o nounset
 DISK_AVAIL=$(df -k . | awk '/[0-9]%/ { gsub(/%/, ""); print $5}')
 if [ ${DISK_AVAIL} -ge 95 ] ; then
     echo "Disk usage is ${DISK_AVAIL}% on $(hostname), not usable";
-    exit 1;
+    exit 101;
 else
    echo "Disk usage is ${DISK_AVAIL}% on $(hostname).";
 fi
@@ -19,16 +19,16 @@ fi
 docker ps >/dev/null
 # Test that docker can allocate 80 and 443, get get busybox
 docker pull busybox:latest >/dev/null
-docker run --rm -t -p 80:80 -p 443:443 -p 1081:1081 -p 1082:1082 -v /$PWD:/tmp/junker99 busybox:latest ls //tmp/junker99 >/dev/null
+docker run --rm -t -p 80:80 -p 443:443 -p 1081:1081 -p 1082:1082 -v /$HOME:/tmp/junker99 busybox:latest ls //tmp/junker99 >/dev/null || exit 102
 
 # Check that required commands are available.
 for command in mysql git go make; do
-    command -v $command >/dev/null || ( echo "Did not find command installed '$command'" && exit 2 )
+    command -v $command >/dev/null || ( echo "Did not find command installed '$command'" && exit 103 )
 done
 
 if [ "$(go env GOOS)" = "windows"  -a "$(git config core.autocrlf)" != "false" ] ; then
  echo "git config core.autocrlf is not set to false on windows"
- exit 3
+ exit 104
 fi
 
 echo "=== testbot $HOSTNAME seems to be set up OK ==="
